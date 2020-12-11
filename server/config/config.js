6f3ee92d3c11f6ef8b086452de2d7cd59ea98151
Esteban <esteban.vame@hotmail.com>
2020-12-10 18:43:18 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,23 @@
 // =========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================
+// Entorno
+// =========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================
+// Base de datos
+// =========================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://adminDB:tPFaKLUf8WF0SqbY@cluster0.nr2vx.mongodb.net/cafe?retryWrites=true&w=majority'; 
+}
+
+process.env.URLDB = urlDB;
