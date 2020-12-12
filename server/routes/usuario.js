ef6280ec7cc0a9ea12382b9dfe9cecced184fa10
Esteban <esteban.vame@hotmail.com>
2020-12-11 19:10:35 -0600

    login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,11 +3,18 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
+const {verificaToken,verificaAdmin_Role} = require ('../middlewares/autenticacion');
 
 const app = express();
 
 
-app.get('/usuario',function(req,res){
+app.get('/usuario', verificaToken,(req,res)=>{
+
+    /* return res.json({
+        usuario: req.usuario,
+        nombre: req.usuario.nombre,
+        email:req.usuario.email
+    }); */
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,7 +43,7 @@ app.get('/usuario',function(req,res){
             });
 });
 
-app.post('/usuario',function(req,res){
+app.post('/usuario',[verificaToken,verificaAdmin_Role],function(req,res){
     let body = req.body;
 
     let usuario = new Usuario({
@@ -73,7 +80,7 @@ app.post('/usuario',function(req,res){
     
 }); //Nuevos registros
 
-app.put('/usuario/:id',function(req,res){
+app.put('/usuario/:id',[verificaToken,verificaAdmin_Role],function(req,res){
     let id = req.params.id;
     //let body = req.body;
     let body = _.pick(req.body, ['nombre','email','img','role','estado']);
@@ -93,7 +100,7 @@ app.put('/usuario/:id',function(req,res){
     
 });  // acutalizar data
 
-app.delete('/usuario/:id',function(req,res){
+app.delete('/usuario/:id',[verificaToken,verificaAdmin_Role],function(req,res){
     let id = req.params.id;
    // let body = _.pick(req.body, ['nombre','email','img','role','estado']);
     //let est = req.params.estado;
